14ba58b054d92b3812636e2ee62b1688f199f3de
Daniel Everett <daniel.everett@gmail.com>
2020-02-16 21:50:02 +1100

    Drop todotxt-parser for manual regex parsing

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ require('dotenv').config();
 const Dropbox = require('dropbox').Dropbox
 const fetch = require('node-fetch')
 const debug = require('debug')('taskviz')
-const todo = require('todotxt-parser')
 const express = require('express')
 const exphb = require('express-handlebars')
 
 const dbclient = new Dropbox({
-	accessToken : process.env.DROPBOX_TOKEN,
-	fetch : fetch
+  accessToken : process.env.DROPBOX_TOKEN,
+  fetch : fetch
 })
 
 const app = express();
@@ -17,30 +16,36 @@ app.engine('handlebars', exphb())
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-	debug('GET /')
-	dbclient.filesGetTemporaryLink({ path : process.env.DROPBOX_FILEPATH })
-		.then((files) => {
-			debug('Dropbox call success, got temp link', files.link)
-			fetch(files.link)
-				.then(fetchres => fetchres.text())
-				.then(text => {
-					debug('Fetch of temp link success %s', text)
-					const tasks = todo.relaxed(text)
-					debug('Task parsing done %O', tasks)
-					res.render('home',  { tasks : tasks })
-				})
-				.catch((error) => {
-					debug('Fetch of temp link failed %O', error)
-					res.render('error', { error_detail : error })
-				})
-		})
-		.catch((error) => {
-			debug('Dropbox call failed %O', error)
-			res.render(
-				'error',
-				{ 'error_detail': error.error }
-			)
-		})
+  debug('GET /')
+  dbclient.filesGetTemporaryLink({ path : process.env.DROPBOX_FILEPATH })
+    .then((files) => {
+      debug('Dropbox call success, got temp link', files.link)
+      fetch(files.link)
+        .then(fetchres => fetchres.text())
+        .then(text => {
+          debug('Fetch of temp link success %s', text)
+          const tasks = { tasks : text.split('\n').map((task) => {
+            debug('Task in map %s', task)
+            const split_task = task.match(/^(?:x\s+\d{4}-\d{2}-\d{2}\s+)?(?:\(([A-za-z]{1})\)\s+)?(?:(@\S+)\s+)?(.*)/i)
+            debug('Task match %o', split_task)
+            if (!split_task) { debug('NO MATCH %s', task) }
+            return { context : split_task[2], task : split_task[3]}
+          })}
+          debug('Map done %O', tasks)
+          res.render('home',  { tasks : tasks })
+        })
+        .catch((error) => {
+          debug('Fetch of temp link, format failed %O', error)
+          res.render('error', { error_detail : error })
+        })
+    })
+    .catch((error) => {
+      debug('Dropbox call failed %O', error)
+      res.render(
+        'error',
+        { 'error_detail': error.error }
+      )
+    })
 })
 
 app.listen(process.env.PORT)
