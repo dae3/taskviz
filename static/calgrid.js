07e001e0f89f9b760ca3e09f65701ab64b8c3979
Daniel Everett <daniel.everett@gmail.com>
2020-02-20 07:27:20 +1100

    WIP

--- static/calgrid.js
+++ static/calgrid.js
@@ -1,15 +1,37 @@
 async function load() {
-  const today = new Date()
+  const curyear = (new Date()).getFullYear()
   const months = Array.apply(undefined, Array(12))
     .map((a,i) => i)
-    .map(a => new Date(today.getFullYear(), a, 1))
-    .map(a=>a.toLocaleDateString(undefined, { month: 'short' }))
-  
+    .map(a => new Date(curyear, a, 1))
+    .map(a => a.toLocaleDateString(undefined, { month: 'short' }))
+
+  const data = await d3.json('/data')
+
+  const tasks = data.map(taskobj => {
+    const [x, y, m, d] = taskobj.date.match(/(\d{4})-(\d{2})-(\d{2})/)
+    if (x) {
+      taskobj.date = new Date(y, m, d)
+      taskobj.month = taskobj.date.getMonth()
+      taskobj.context = taskobj.context ? taskobj.context.replace('@','') : ''
+      return taskobj 
+    } else {
+      return null
+    }
+  }).filter(a=>a)
+
+  const tasksbymonth = tasks.reduce((a,v,i) => {
+    a[v.month].push(v)
+    return a
+  }, months.map(a=>[]))
+
   var cal = d3.select("body").select("div#cal").selectAll("div.mbox")
     .data(months)
-    .text(d=>d)
-
-  cal.enter().append("div")
-    .classed("mbox", true)
-    .text(d=>d)
+    .join("div")
+    .attr("class", "mbox").attr('month', (d,i) => i)
+    .text(d => d)
+    .selectAll("span.taskdot")
+    .data((d,i) => tasksbymonth[i])
+    .join("span")
+    .attr("class", "taskdot")
+    .text(d => d.context)
 }
