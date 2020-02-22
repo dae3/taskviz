57492d141ff91c463084e2fc2fa1039b202eec18
Daniel Everett <daniel.everett@gmail.com>
2020-02-22 18:10:57 +1100

    Add bad data filter, new hier generator, text clipping

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-'use strict';
-require('dotenv').config();
+'use strict'
+require('dotenv').config()
 const Dropbox = require('dropbox').Dropbox
 const fetch = require('node-fetch')
 const express = require('express')
@@ -11,101 +11,105 @@ const path = require('path')
 const filecache = new cache()
 
 const dbclient = new Dropbox({
-	accessToken : process.env.DROPBOX_TOKEN,
-	fetch : fetch
+  accessToken: process.env.DROPBOX_TOKEN,
+  fetch: fetch
 })
 
-const app = express();
+const app = express()
 app.engine('handlebars', exphb())
 app.set('view engine', 'handlebars')
 
-function getData(req) {
-	return fetch(new URL(`${req.protocol}://${req.hostname}:${req.client.localPort}/data`)).then(res => res.json())
+function getData (req) {
+  return fetch(new URL(`${req.protocol}://${req.hostname}:${req.client.localPort}/data`)).then(res => res.json())
 }
 
 app.get('/', (req, res) => {
-	const debug = require('debug')('taskviz')
-	getData(req).then(data => {
-		debug('Success from /data %O', data)
-		res.render('home',  { tasks : data.filter(a => a.date != null && a.context != null) })
-	})
-		.catch((err) => {
-			debug('Error from /data %s', err)
-			res.render('error', { error_detail : err })
-		})
+  const debug = require('debug')('taskviz')
+  getData(req).then(data => {
+    debug('Success from /data %O', data)
+    res.render('home', { tasks: data.filter(a => a.date != null && a.context != null) })
+  })
+    .catch((err) => {
+      debug('Error from /data %s', err)
+      res.render('error', { error_detail: err })
+    })
 })
 
-app.get('/calgrid', (req, res) => {
-	const debug = require('debug')('taskviz')
-	res.render('calgrid')
+app.get('/calgrid', (_req, res) => {
+  res.render('calgrid')
 })
 
-app.get('/viz', (req, res) => {
-	const debug = require('debug')('taskviz')
-	res.render('vizhome')
+app.get('/viz', (_req, res) => {
+  res.render('vizhome')
 })
 
 app.get('/static/:asset', (req, res) => {
   const debug = require('debug')('taskviz:static')
-  if (!req.params.asset) { debug('400'); res.status(400).end('Missing asset parameter') }
-  else if (!req.params.asset.match('[A-Za-z0-9\.]')) { debug(`400 ${req.params.asset}`); res.status(400).end('Invalid asset name') }
-  else {
+  if (!req.params.asset) {
+    debug('400'); res.status(400).end('Missing asset parameter')
+  } else if (!req.params.asset.match('[A-Za-z0-9\.]')) {
+    debug(`400 ${req.params.asset}`); res.status(400).end('Invalid asset name')
+  } else {
     res.status(200)
     debug(`Asset param OK, attempting stream of ${req.params.asset}`)
-    var s = fs.createReadStream(path.format({ dir: process.env.STATIC_PATH, base: req.params.asset }))
+    const filepath = path.format({ dir: process.env.STATIC_PATH, base: req.params.asset })
+    const s = fs.createReadStream(filepath)
     s.on('error', e => {
-      if (e.code == 'ENOENT') { debug('404'); res.status(404).end() }
-      else { debug(`500 ${err}`); res.status(500).end() }
+      if (e.code === 'ENOENT') {
+        debug(`404 on ${filepath}`); res.status(404).end()
+      } else {
+        debug(`500 ${e}`); res.status(500).end()
+      }
     })
     s.pipe(res)
   }
 })
 
-function getFile(filename) {
-	var cachedfile = filecache.get(filename)
-	const debug = require('debug')('taskviz:cache')
-	if (!cachedfile) {
-		debug(`Cache miss on ${filename}`)
-		return new Promise((resolve, reject) => {
-			dbclient.filesGetTemporaryLink({ path : filename })
-				.then(files => { debug('Dropbox link fetch success'); return fetch(files.link) })
-				.then(res => { debug('Fetch of temp link success'); return res.text() })
-				.then(text => {
-					debug('Text of temp link fetch success')
-					filecache.set(filename, text)
-					resolve(text)
-				})
-		})
-	} else {
-		debug(`Cache hit on ${filename}`)
-		return Promise.resolve(cachedfile)
-	}
+function getFile (filename) {
+  var cachedfile = filecache.get(filename)
+  const debug = require('debug')('taskviz:cache')
+  if (!cachedfile) {
+    debug(`Cache miss on ${filename}`)
+    return new Promise((resolve, reject) => {
+      dbclient.filesGetTemporaryLink({ path: filename })
+        .then(files => { debug('Dropbox link fetch success'); return fetch(files.link) })
+        .then(res => { debug('Fetch of temp link success'); return res.text() })
+        .then(text => {
+          debug('Text of temp link fetch success')
+          filecache.set(filename, text)
+          resolve(text)
+        })
+    })
+  } else {
+    debug(`Cache hit on ${filename}`)
+    return Promise.resolve(cachedfile)
+  }
 }
 
-app.get('/data', (req, res) => {
-	const debug = require('debug')('taskviz:api')
-	getFile(process.env.DROPBOX_FILEPATH)
-		.then(text => {
-			debug('Fetch of file success %s', text)
-			const tasks =  text.split('\n').map((task) => {
-				debug('Task in map %s', task)
-				const [full, done_date, priority, context, text] =
-					task.match(/^(?:x\s+(\d{4}-\d{2}-\d{2})\s+)?(?:\(([A-za-z]{1})\)\s+)?(?:(@\S+)\s+)?(.*)/i)
-				if (full) {
-					debug('Task match %o', full)
-					return { context : context, task : text, date : done_date }
-				} else {
-					debug('Task regex didn\'t match %s', full)
-					return null
-				}
-			}).filter(a => a != null)
-			debug('Map done %O', tasks)
-			res.status(200).json(tasks)
-		})
-		.catch((error) => {
-			debug('Fetch of temp link, format failed %O', error)
-			res.status(500).json(error)
-		})
+app.get('/data', (_req, res) => {
+  const debug = require('debug')('taskviz:api')
+  getFile(process.env.DROPBOX_FILEPATH)
+    .then(text => {
+      debug('Fetch of file success')
+      const tasks = text.split('\n').map((task) => {
+        const regmatch = task.match(/^(?:x\s+(\d{4})-(\d{2})-(\d{2})\s+)(?:\(([A-za-z]{1})\)\s+)?(?:@(\S+)\s+)(.*)/i)
+        if (regmatch) {
+          const [full, year, month, day, priority, context, text] = regmatch
+          debug('Task match %o', full)
+          const project = text.match(/\+(\w+)/)
+          return { context: context, priority: priority, project: project ? project[1] : null, task: text, date: new Date(year, month, day) }
+        } else {
+          debug('Task regex didn\'t match %s', task)
+          return null
+        }
+      }).filter(a => a != null)
+      debug('Map done %O', tasks)
+      res.status(200).json(tasks)
+    })
+    .catch((error) => {
+      debug('Fetch of temp link, format failed %O', error)
+      res.status(500).json(error)
+    })
 })
 
 app.listen(process.env.PORT)
--- static/calgrid.js
+++ static/calgrid.js
@@ -1,46 +1,43 @@
-async function load() {
-  const curyear = (new Date()).getFullYear()
-  const months = Array.apply(undefined, Array(12))
-    .map((a,i) => i)
-    .map(a => new Date(curyear, a, 1))
-    .map(a => { return {
-      month: a.toLocaleDateString(undefined, { month: 'short' }),
-      contexts: []
-    }})
+async function load () {
+  const tasks = await d3.json('/data')
 
-  const data = await d3.json('/data')
-
-  const tasks = data.map(taskobj => {
-    const [x, y, m, d] = taskobj.date.match(/(\d{4})-(\d{2})-(\d{2})/)
-    if (x) {
-      taskobj.date = new Date(y, m, d)
-      taskobj.month = taskobj.date.getMonth()
-      taskobj.context = taskobj.context ? taskobj.context.replace('@', '') : ''
-      return taskobj
-    } else {
-      return null
+  // template hierarchichal data structure for d3.treemap
+  // children array elements are months
+  // children.children ... are context
+  // children.children.children ... are tasks
+  class HierTemplate {
+    constructor (parent, id, data) {
+      this.parent = parent
+      this.data = data
+      this.id = id
+      this.children = []
     }
-  }).filter(a=>a).filter(task => !task.task.match(/^x/)).filter(task => !task.task.match(/@/))
+  }
 
-  var root = []
+  // create root and populate all months
+  const root = new HierTemplate(null, 'root')
+  const monthName = (monthNum) => {
+    const curYear = (new Date()).getFullYear()
+    return (new Date(curYear, monthNum, 1).toLocaleDateString(undefined, { month: 'short' }))
+  }
+  root.children = Array.apply(undefined, Array(12)).map((_, i) => new HierTemplate(root.id, i, monthName(i)))
+
+  // insert tasks into hierarchy
   tasks
-    .forEach(task => {
-      root.push( { name: task.task, parent: `${task.context}${task.month}` })
-      root.push( { name: `${task.context}${task.month}`, parent: task.month })
-      root.push( { name: task.month, parent: 'tasks' } )
-    })
-
-  root.push( { name: 'tasks', parent: '' } )
-  root = root
-    .reduce((unique, task) => {
-      if (!unique.some(t => (t.name == task.name && t.parent == task.parent))) {
-        unique.push(task)
+    .map(task => { task.date = new Date(task.date); return task })
+    .forEach(
+      task => {
+        const monthElement = root.children[task.date.getMonth()]
+        var contextElement = monthElement.children.find(m => m.data === task.context)
+        if (!contextElement) {
+          contextElement = new HierTemplate(monthElement.id, `${monthElement.id}${task.context}`, task.context)
+          monthElement.children.push(contextElement)
+        }
+        contextElement.children.push(new HierTemplate(contextElement.id, 'dummyid', task))
       }
-      return unique
-    }, [])
+    )
 
-  const taskdata = d3.stratify().id(d => d.name).parentId(d => d.parent)(root).count()
-  const treedata = d3.treemap().size([500, 500])(taskdata)
+  const treedata = d3.treemap().size([500, 500])(d3.hierarchy(root).count())
 
   // group for each month
   const monthgroup = d3.select('svg#cals').selectAll('g.month')
@@ -56,9 +53,9 @@ async function load() {
 
   monthgroup
     .append('text')
-    .text(d => d.data.name)
-    .attr('x', 10)
-    .attr('y', 20)
+    .text(d => d.data.data)
+    .attr('x', d => (d.x1 - d.x0) / 2)
+    .attr('y', d => (d.y1 - d.y0) / 2)
 
   // each context
   const contextgroup = monthgroup
@@ -71,18 +68,28 @@ async function load() {
 
   contextgroup
     .append('rect')
+    .attr('id', d => `rect${d.ancestors()[0].data.id}`)
     .attr('x', d => (d.x0 - d.ancestors()[1].x0) + PADDING)
     .attr('y', d => (d.y0 - d.ancestors()[1].y0) + PADDING)
     .attr('width', d => (d.x1 - d.x0) - PADDING * 2)
     .attr('height', d => (d.y1 - d.y0) - PADDING * 2)
 
-  const TEXTOFFSET = PADDING + 10
+  // clip path for text, using the context rect
+  contextgroup
+    .append('clipPath')
+    .attr('id', d => `clip${d.ancestors()[0].data.id}`)
+    .append('use')
+    .attr('href', d => `#rect${d.ancestors()[0].data.id}`)
+
+  const TEXTOFFSETX = PADDING + 2
+  const TEXTOFFSETY = PADDING + 9
 
   contextgroup
     .append('text')
-    .text(d => d.data.name.replace(/\d+$/, ''))
-    .attr('x', d => d.x0 - d.ancestors()[1].x0 + TEXTOFFSET)
-    .attr('y', d => d.y0 - d.ancestors()[1].y0 + TEXTOFFSET)
+    .attr('clip-path', d => `url(#clip${d.ancestors()[0].data.id})`)
+    .text(d => `${d.data.data} ${d.value}`)
+    .attr('x', d => d.x0 - d.ancestors()[1].x0 + TEXTOFFSETX)
+    .attr('y', d => d.y0 - d.ancestors()[1].y0 + TEXTOFFSETY)
 }
 
 load()
